15aa6fb6034bf9f6e6d4385f94f8ce625d36291a
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2020-03-22 22:13:31 +0200

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor() {
         this.key = 'GJjwHvRrcoS2i2Gl7NpFuX63A3IEezVM';
-        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
     }
     async updateCity(city) {
         const cityDetails = await this.getCity(city);
